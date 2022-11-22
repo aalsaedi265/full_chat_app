c58c427554059d2b5e508f1fc533efecfae563c6
aalsaedi265 <aalsaedi265@gmail.com>
2022-11-22 09:46:38 -0600

    commited out extra code

--- chatBot_rasa/actions/actions.py
+++ chatBot_rasa/actions/actions.py
@@ -7,27 +7,51 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-from typing import Any, Text, Dict, List
+# from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
 
 
-class ActionHelloWorld(Action):
+# class ActionMusic(Action):
 
-     def name(self) -> Text:
-         return "action_intro_show_video"
+#      def name(self) -> Text:
+#          return "action_music"
 
-     def run(self, dispatcher: CollectingDispatcher,
-             tracker: Tracker,
-             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#      def run(self, dispatcher: CollectingDispatcher,
+#              tracker: Tracker,
+#              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
        
-         msg = { "type": "video", "payload": { "title": "Link name", "src": "https://www.youtube.com/watch?v=6IeAiaPNw7s" } }
+#          msg = { "type": "video",
+#                 "payload": { "title": "Link name",
+#                             "src": "https://www.youtube.com/watch?v=6IeAiaPNw7s" 
+#                             } 
+#                 }
 
-         dispatcher.utter_message(text="Check this video: ",attachment=msg)
+#          dispatcher.utter_message(template = "utter_music",text="Check this video: ",attachment=msg)
          
-         return []
+#          return []
+     
+
+# class AskLeaveType(Action):
+
+#      def name(self) -> Text:
+#          return "askLeaveType"
+
+#      def run(self, dispatcher: CollectingDispatcher,
+#              tracker: Tracker,
+#              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#         data= [{ "title": "Sick Leave", "description": "Sick leave is time off from work that workers can use to stay home to address their health and safety needs without losing pay." },
+#                { "title": "Earned Leave", "description": "Earned Leaves are the leaves which are earned in the previous year and enjoyed in the preceding years. " },
+#                { "title": "Casual Leave", "description": "Casual Leave are granted for certain unforeseen situation or were you are require to go for one or two days leaves to attend to personal matters and not for vacation." },
+#                { "title": "Flexi Leave", "description": "Flexi leave is an optional leave which one can apply directly in system at lease a week before." } 
+#                ]
+
+#         message={ "payload": "collapsible", "data": data }
+
+#         dispatcher.utter_message(text="You can apply for below leaves",json_message=message)
 
 
 
